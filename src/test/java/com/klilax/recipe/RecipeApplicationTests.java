7b7b8fedee83f58489dd8a301277877324a638db
[Khalil Ahmed] <[klilax@hotmail.co.uk]>
2021-11-21 06:13:56 +0300

    spring init

---
+++ src/test/java/com/klilax/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klilax.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
